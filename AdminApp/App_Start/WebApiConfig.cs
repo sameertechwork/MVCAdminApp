440c8e8b1e5c0bc7e4533f568d25771499cebfa5
sameer <sameer.inapp@gmail.com>
2017-06-27 16:27:00 +0530

    wrapping class added

--- AdminApp/App_Start/WebApiConfig.cs
+++ AdminApp/App_Start/WebApiConfig.cs
@@ -1,7 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using AdminApp.Utility;
+
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.Routing;
 
 namespace AdminApp
 {
@@ -9,11 +17,25 @@ namespace AdminApp
     {
         public static void Register(HttpConfiguration config)
         {
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            //// Web API routes
+            //config.MapHttpAttributeRoutes();
+                        
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+            config.MessageHandlers.Add(new WrappingHandler());
+
+            c
         }
     }
 }
---
+++ AdminApp/Utility/WrappingHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace AdminApp.Utility
+{
+    public class WrappingHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = await base.SendAsync(request, cancellationToken);
+
+            return BuildApiResponse(request, response);
+        }
+
+        private static HttpResponseMessage BuildApiResponse(HttpRequestMessage request, HttpResponseMessage response)
+        {
+            object content;
+            HttpResponseMessage obj_Response;
+            ResponseClass obj_ResponseClass = new ResponseClass();
+
+            if (response.TryGetContentValue(out content) && !response.IsSuccessStatusCode)
+            {
+                HttpError error = content as HttpError;
+
+                if (error != null)
+                {
+                    obj_ResponseClass.Status = 0;
+                    obj_ResponseClass.Message = error.Message;
+                }
+            }
+            else
+            {
+                List<object> responseData = new List<object>();
+                responseData.Add((content == null ? new object() : content));
+                obj_ResponseClass.data = responseData;
+                obj_ResponseClass.Status = 1000;
+                obj_ResponseClass.Message = "Success";
+
+            }
+            obj_Response = request.CreateResponse<ResponseClass>(response.StatusCode, obj_ResponseClass);
+            return obj_Response;
+        }
+
+        public class ResponseClass
+        {
+            public int Status { get; set; }
+            public string Message { get; set; }
+            public List<object> data { get; set; }
+        }
+    }
+}
